53d0d257b1b449b6f3046ec2e4cd4938b9b7fdbe
Mohammad Javadi <mjavadi@sabine.rcdc.uh.edu>
2021-03-13 22:20:44 -0600

    Change file format

--- test.py
+++ test.py
@@ -15,22 +15,27 @@ import numpy as np
 from numpy import array
 import os
 from keras.models import load_model
+
 from scipy.misc import imresize
 import argparse
+import sys
+
 
 import Utils, Utils_model
 from Utils_model import VGG_LOSS
 
 image_shape = (96,96,3)
 
+
+
 def test_model(input_hig_res, model, number_of_images, output_dir):
     
-    x_test_lr, x_test_hr = Utils.load_test_data_for_model(input_hig_res, 'jpg', number_of_images)
+    x_test_lr, x_test_hr = Utils.load_test_data_for_model(input_hig_res, 'png', number_of_images)
     Utils.plot_test_generated_images_for_model(output_dir, model, x_test_hr, x_test_lr)
 
 def test_model_for_lr_images(input_low_res, model, number_of_images, output_dir):
 
-    x_test_lr = Utils.load_test_data(input_low_res, 'jpg', number_of_images)
+    x_test_lr = Utils.load_test_data(input_low_res, 'png', number_of_images)
     Utils.plot_test_generated_images(output_dir, model, x_test_lr)
 
 if __name__== "__main__":
@@ -49,7 +54,7 @@ if __name__== "__main__":
     parser.add_argument('-m', '--model_dir', action='store', dest='model_dir', default='./model/gen_model3000.h5' ,
                     help='Path for model')
                     
-    parser.add_argument('-n', '--number_of_images', action='store', dest='number_of_images', default=25 ,
+    parser.add_argument('-n', '--number_of_images', action='store', dest='number_of_images', default=2 ,
                     help='Number of Images', type=int)
                     
     parser.add_argument('-t', '--test_type', action='store', dest='test_type', default='test_model',
