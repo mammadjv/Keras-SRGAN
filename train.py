3500587876a9819bb662d685d90aee258c78af10
mammadjv <mohammadjv94@gmail.com>
2021-11-15 09:03:33 -0600

    Changed the loss function

--- train.py
+++ train.py
@@ -9,7 +9,7 @@
 
 from Network import Generator, Discriminator
 import Utils_model, Utils
-from Utils_model import VGG_LOSS
+from Utils_model import MSE_loss
 
 from keras.models import Model
 from keras.layers import Input, Concatenate
@@ -35,7 +35,7 @@ def get_gan_network(discriminator, shape, generator, optimizer, vgg_loss):
 
     gan_output = discriminator(x)
     gan = Model(inputs=gan_input, outputs=[x,gan_output])
-    gan.compile(loss=[vgg_loss, "binary_crossentropy"],
+    gan.compile(loss=[MSE_loss, "binary_crossentropy"],
                 loss_weights=[1., 1e-3],
                 optimizer=optimizer, run_eagerly=True)
 
@@ -82,68 +82,38 @@ def validate(gan, generator, discriminator, x_validate_hr, x_validate_lr, batch_
 def train(epochs, batch_size, input_dir, output_dir, model_save_dir, number_of_images, train_test_ratio):
     
     x_train_lr, x_train_hr, x_test_lr, x_test_hr, x_validate_hr, x_validate_lr = Utils.load_training_data(input_dir, '.png', number_of_images, train_test_ratio) 
-    loss = VGG_LOSS(image_shape)
+    loss = MSE_loss(image_shape)
     
     batch_count = int(x_train_hr.shape[0] / batch_size)
     shape = (image_shape[0]//downscale_factor, image_shape[1]//downscale_factor, 3)
     generator = Generator(shape).generator()
-    discriminator = Discriminator(image_shape).discriminator()
 
     optimizer = Utils_model.get_optimizer()
-    generator.compile(loss=loss.vgg_loss, optimizer=optimizer, run_eagerly=True)
-    discriminator.compile(loss="binary_crossentropy", optimizer=optimizer, run_eagerly=True)
-     
-    gan = get_gan_network(discriminator, shape, generator, optimizer, loss.vgg_loss)
+    generator.compile(loss=loss.mse_loss, optimizer=optimizer, run_eagerly=True)
+
     loss_file = open(model_save_dir + 'losses.txt' , 'w+')
     loss_file.close()
     
-    discriminator_loss = 0
     gan_loss = 0
     for e in range(1, epochs+1):
         print ('-'*15, 'Epoch %d' % e, '-'*15)
         for _ in tqdm(range(batch_count)):
-            
-            rand_nums = np.random.randint(0, x_train_hr.shape[0], size=batch_size)
-            
-            image_batch_hr = x_train_hr[rand_nums]
-            image_batch_lr = x_train_lr[rand_nums]
-            generated_images_sr = generator.predict(image_batch_lr)
-            generated_images_sr = Concatenate(axis=3)([generated_images_sr, generated_images_sr, generated_images_sr])
-
-            real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
-            fake_data_Y = np.random.random_sample(batch_size)*0.2
-            
-            discriminator.trainable = True
-            
-            d_loss_real = discriminator.train_on_batch(image_batch_hr, real_data_Y)
-            d_loss_fake = discriminator.train_on_batch(generated_images_sr, fake_data_Y)
-            discriminator_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
-            
             rand_nums = np.random.randint(0, x_train_hr.shape[0], size=batch_size)
             image_batch_hr = x_train_hr[rand_nums]
             image_batch_lr = x_train_lr[rand_nums]
+            generator_loss = generator.train_on_batch(image_batch_lr, image_batch_hr)
 
-            gan_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
-            discriminator.trainable = False
-            gan_loss = gan.train_on_batch(image_batch_lr, [image_batch_hr,gan_Y])
-
-
-        validate_gan_loss, validate_discriminator_loss = validate(gan, generator, discriminator, x_validate_hr, x_validate_lr)
-        print("discriminator_loss : %f" % discriminator_loss)
-        print("gan_loss :", gan_loss)
-        gan_loss = str(gan_loss)
+        print("gan_loss :", generator_loss)
 
         loss_file = open(model_save_dir + 'losses.txt' , 'a')
-        loss_file.write('epoch%d : \ntrain_gan_loss = %s ; train_disc_loss = %f\n' %(e, gan_loss, discriminator_loss) )
-        loss_file.write('val_gan_loss = %s ; val_disc_loss = %f\n' %(validate_gan_loss, validate_discriminator_loss) )
+        loss_file.write('epoch%d : \ntrain_gan_loss = {}\n' .format(e, generator_loss))
         loss_file.close()
 
         if e == 1 or e % 5 == 0:
             Utils.plot_generated_images(output_dir, e, generator, x_test_hr, x_test_lr)
 
-        if e % 40 == 0:
+        if e == 1 or e % 40 == 0:
             generator.save(model_save_dir + 'gen_model%d.h5' % e)
-            discriminator.save(model_save_dir + 'dis_model%d.h5' % e)
 
 
 if __name__== "__main__":
@@ -153,16 +123,16 @@ if __name__== "__main__":
     parser.add_argument('-i', '--input_dir', action='store', dest='input_dir', default='./data_synthetic/' ,
                     help='Path for input images')
                     
-    parser.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output/' ,
+    parser.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output3/' ,
                     help='Path for Output images')
     
-    parser.add_argument('-m', '--model_save_dir', action='store', dest='model_save_dir', default='./model/' ,
+    parser.add_argument('-m', '--model_save_dir', action='store', dest='model_save_dir', default='./model3/' ,
                     help='Path for model')
 
     parser.add_argument('-b', '--batch_size', action='store', dest='batch_size', default=8,
                     help='Batch Size', type=int)
                     
-    parser.add_argument('-e', '--epochs', action='store', dest='epochs', default=80 ,
+    parser.add_argument('-e', '--epochs', action='store', dest='epochs', default=1000 ,
                     help='number of iteratios for trainig', type=int)
                     
     parser.add_argument('-n', '--number_of_images', action='store', dest='number_of_images', default=450 ,
